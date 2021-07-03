2fad1d6dc3cc453c35ab5575822f0f5b9149c5dc
Arthur Zyabirov <zyabirow@gmail.com>
2021-07-03 15:09:11 +0300

    first commit

---
+++ db/src/main/java/Db.java
@@ -0,0 +1,26 @@
+import java.util.UUID;
+
+public class Db {
+
+    private DbSetting1 dbSetting;
+    private MyEntity myEntity;
+
+    public Db(DbSetting1 dbSetting) {
+        this.dbSetting = dbSetting;
+        myEntity = new MyEntity("first");
+        myEntity.setId(UUID.randomUUID());
+    }
+
+    public DbSetting1 getDbSetting() {
+        return dbSetting;
+    }
+
+    public MyEntity getMyEntity() {
+        return myEntity;
+    }
+
+    public void setMyEntity(MyEntity myEntity) {
+        this.myEntity = myEntity;
+    }
+}
+
---
+++ db/src/main/java/DbSetting1.java
@@ -0,0 +1,11 @@
+public class DbSetting1 {
+
+    private String name;
+    private String password;
+
+    public DbSetting1(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
